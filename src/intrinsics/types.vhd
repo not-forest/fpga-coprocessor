0e7d1f69d40eb46ba50dde9dab98f0eb0d9d3598
not-forest <notforest69@gmail.com>
2025-03-23 20:00:39 +0100

    Added more types related to communication protocol.

--- src/intrinsics/types.vhd
+++ src/intrinsics/types.vhd
@@ -29,12 +29,41 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package intrinsics is 
-    type t_command_type is  -- Used to parse the current command type between all available ones. 
-        (MUL);
+    constant PARALLEL_BUS_WIDTH : natural := 8;
 
-    type t_Command is record
-        ctype : t_command_type;
+    type t_command_type is      -- Used to parse the current command type between all available ones. 
+        -- Communication related commands:
+        -- INIT (Used to prepare the FPGA coprocessor before sending proper commands.)
+        -- ACK  (Used to acknowledge the FPGA's ACK request within the status line)
+        -- WAKE (Forces the coprocessor to exit sleep mode.)
+        --
+        -- Computational commands:
+        -- MUL (Performs matrix multiplication. Expects next two bytes to be the size of the expected output matrix.)
+        (INIT, ACK, WAKE, MUL);
+
+    type t_status is            -- Used to provide the current status of FPGA for the master microcontroller.
+        -- UNDEF (Undefined state of the coprocessor. It can have some trash within and must be reinitialized first.)
+        -- SLEEP (Coprocessor is put in low power sleep mode, to not loose cycles for nothing. Must be woke up first.)
+        -- IDLE (Waiting for the new command.)
+        -- PROC (Performing tasks.)
+        -- ACK (Waiting for ACK command.)
+        (UNDEF, SLEEP, IDLE, PROC, ACK);
+
+    -- Defines a parallel bus to interface the master microcontroller. 
+    type t_ParallelBus is record
+        d_in        : std_logic_vector(PARALLEL_BUS_WIDTH - 1 downto 0);
+        cmd         : std_logic;
+        d_out       : std_logic_vector(PARALLEL_BUS_WIDTH - 1 downto 0);
+        c_status    : t_status; 
     end record;
+
+    -- Default initialization values of the parallel bus.
+    constant C_PBUS_INIT : t_ParallelBus := (
+        d_in => (others => '0'),
+        d_out => (others => '0'),
+        c_status => UNDEF,
+        cmd => '0'
+    );
 
     -- Custom pipelining procedure, to pipeline any type of data for one clock cycle. 
     procedure pipeline (
