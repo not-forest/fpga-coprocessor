0d165b27c49ae82fde167d5ad4c8b03e7b71fef1
not-forest <notforest69@gmail.com>
2025-07-13 21:24:18 +0000

    Added SPI bus probing ready for communication with FPGA Coprocessor.

--- driver/spi.c
+++ driver/spi.c
@@ -1,20 +1,68 @@
 /* 
  *  File: spi.c
- *  Desc: SPI bus initialization module. 
+ *  Desc: SPI bus initialization and control module. 
  *
  *  Tested on Raspberry Pi 4 with Linux kernel 5.10  
  * */
 
 #include <linux/module.h>
 #include <linux/spi/spi.h>
+#include <linux/mutex.h>
 
 #include "coproc.h"
 
+#define SPI_WORD_BITS 8
+
+/* SPI device wrapper for mutual exclusion between kernel calls. */
+struct spi_net {
+    struct spi_device *spi;
+    struct mutex lock;
+};
+
+/* Writes word of data to the SPI bus.  */
+static int coproc_send_word(struct spi_net *net, u8 *wr_buf) {
+    int ret;
+
+    mutex_lock(&net->lock);
+    ret = spi_write(net->spi, wr_buf, SPI_WORD_BITS);
+    mutex_unlock(&net->lock);
+    return ret;
+}
+
+/* Reads word of data from the SPI bus.  */
+static int coproc_read_word(struct spi_net *net, u8 *rd_buf) { 
+    int ret;
+
+    mutex_lock(&net->lock);
+    ret = spi_read(net->spi, rd_buf, SPI_WORD_BITS);
+    mutex_unlock(&net->lock);
+    return ret;
+}
+
+/* Allocates memory for SPI device net. */
 static int coproc_spi_probe(struct spi_device *spi) {
+    struct spi_net *net;
+    dev_info(&spi->dev, "Probing FPGA Coprocessor SPI bus...");
+
+    net = kzalloc(sizeof(struct spi_net), GFP_KERNEL);
+    if (!net)
+        return -ENOMEM;
+
+    net->spi = spi;
+    mutex_init(&net->lock);
+
+    spi_set_drvdata(spi, net);
+
     return 0;
 }
 
 static int coproc_spi_remove(struct spi_device *spi) {
+    struct spi_net *net;
+    dev_info(&spi->dev, "FPGA Coprocessor SPI bus release...");
+
+    net = spi_get_drvdata(spi);
+    kfree(net);
+
     return 0;
 }
 
